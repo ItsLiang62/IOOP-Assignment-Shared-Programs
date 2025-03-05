a0392b5c7594a71a7c272ac575af9123bb3b5c5a
ItsLiang62 <andersonwlx62@gmail.com>
2025-03-05 16:22:05 +0800

    first commit from local repo

---
+++ SQLQuery2.sql
@@ -0,0 +1,19 @@
+﻿CREATE TRIGGER chef_employee_id_only
+ON ItemOrder
+INSTEAD OF INSERT
+AS
+BEGIN
+    IF EXISTS (
+        SELECT 1 
+        FROM inserted i
+        LEFT JOIN Employee e ON i.ChefEmployeeID = e.EmployeeID
+        WHERE e.Position <> 'Chef' OR e.Position IS NULL
+    )
+        THROW 50000, 'Error: ChefEmployeeID must reference an employee with the Chef position', 1;
+    ELSE
+    BEGIN
+        -- If the condition is met, insert the row
+        INSERT INTO ItemOrder (OrderID, ItemNumber, CustomerID, ChefEmployeeID, DateOfOrder, OrderStatus)
+        SELECT OrderID, ItemNumber, CustomerID, ChefEmployeeID, DateOfOrder, OrderStatus FROM inserted;
+    END;
+END;
---
+++ SQLQuery4.sql
@@ -0,0 +1,1 @@
+﻿drop table ItemOrder, Item, Customer, Employee
